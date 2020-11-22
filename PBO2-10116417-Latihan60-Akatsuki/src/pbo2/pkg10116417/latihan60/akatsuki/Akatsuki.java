04d42937e5508dad3ac8e7fed7536f0190e561ea
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-22 12:37:28 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan60-Akatsuki/src/pbo2/pkg10116417/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan60.akatsuki;
+
+/**
+ *
+ */
+public class Akatsuki {
+    
+    protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+}
---
+++ PBO2-10116417-Latihan60-Akatsuki/src/pbo2/pkg10116417/latihan60/akatsuki/Anggota.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan60.akatsuki;
+
+/**
+ *
+ * 
+ */
+public class Anggota extends Akatsuki{
+    
+     private final String nama;
+    private final String asalDesa;
+    private final String senjata;
+    private final String tipeKarakter;
+    private final String jurusAndalan;
+    private final String tewas;
+    private final String cincin;
+    private final boolean havePartner;
+
+    public Anggota(String nama, String asalDesa, String senjata, String tipeKarakter, 
+                  String jurusAndalan, String tewas, String cincin, boolean 
+                  havePartner, String pakaian, String tujuan) {
+        super(pakaian, tujuan);
+        this.nama = nama;
+        this.asalDesa = asalDesa;
+        this.senjata = senjata;
+        this.tipeKarakter = tipeKarakter;
+        this.jurusAndalan = jurusAndalan;
+        this.tewas = tewas;
+        this.cincin = cincin;
+        this.havePartner = havePartner;
+        
+    }
+    
+    public String getnama() {
+        return nama;
+    }
+    
+    public String getsenjata() {
+        return senjata;
+    }
+
+    public String gettipeKarakter() {
+        return tipeKarakter;
+    }
+
+    public String getjurusAndalan() {
+        return jurusAndalan;
+    }
+
+    public String gettewas() {
+        return tewas;
+    }
+
+    public boolean isHavePartner() {
+        return havePartner;
+    }
+    
+    public void displayAnggota() {
+        System.out.println("-------------------");
+        System.out.println("Nama : " + nama);
+        System.out.println("-------------------");
+        System.out.println("Asal : " + asalDesa);
+        System.out.println("Senjata : " + senjata);
+        System.out.println("Character Type : " + tipeKarakter);
+        System.out.println("Jurus Andalan : " + jurusAndalan);
+        System.out.println("Cincin yang digunakan : " + cincin);
+        System.out.println("Kematian : " + tewas);
+        if(isHavePartner()) {
+            System.out.println("Mission Type : Team");
+        } else {
+            System.out.println("Mission Type : Solo");
+        }
+        System.out.println();
+    }
+    
+}
